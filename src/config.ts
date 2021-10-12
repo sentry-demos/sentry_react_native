153611fdb743fbad12cf2d8f7cad9d3b587b912c
Yuval Nachon <yuval.nachon@sentry.io>
2021-10-12 14:41:44 -0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 export const DSN =
-  'https://3d19b89ce4774ad2b429b09a9375fbbb@o87286.ingest.sentry.io/5716557';
+  'https://db5ca84ef1154d3bbfb83f4769888954@o87286.ingest.sentry.io/6004353';
 
 export const BACKEND_URL = 
   'https://application-monitoring-flask-dot-sales-engineering-sf.appspot.com'
   // 'http://127.0.0.1:8080' // for local flask backend
+
+export const SENTRY_AUTH_TOKEN = 'd842af355a46460a84047176823d5de23a45cc2f49dd4a339dbcb72f7a0ec1f0';
