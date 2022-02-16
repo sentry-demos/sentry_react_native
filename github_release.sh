3b22e796b193021e881ee2d4f38352ce751c77a1
William Capozzoli <thinkocapo@gmail.com>
2022-02-15 16:50:06 -0800

    check SE tag before apk upload

--- github_release.sh
+++ github_release.sh
@@ -2,6 +2,15 @@ PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 REPO=sentry-demos/sentry_react_native
 
 while true; do
+    read -p "Is the right value set for SE tag in .env? Answer y/n: " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y or n.";;
+    esac
+done
+
+while true; do
     read -p "Do you wish to create Github Release $PACKAGE_VERSION for $REPO? Answer y/n: " yn
     case $yn in
         [Yy]* ) make install; break;;
